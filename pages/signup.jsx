85dd91073fb86566bae7cd263113291464f9017e
best <gt@best.com>
2022-09-22 18:08:28 +0100

    fixed

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -8,14 +8,14 @@ const signup = (props) => {
     <div className="flex items-center w-full bg-slate-100 sm:min-h-screen">
       <div className="flex-1 h-full max-w-5xl bg-white mx-auto rounded-lg shadow-xl">
         <div className="flex flex-col md:flex-row">
-          <div className="h-32 md:h-[610px] md:w-1/2 py-16 px-10 md:flex hidden">
+          <div className="h-full md:h-[610px] md:w-1/2 p-16 px-10 md:flex hidden">
             <Image src={image} alt="" className="object-center w-full h-full" />
           </div>
 
           {/*  */}
-          <div className="border rounded-l-3xl items-center text-center max-w-4xl py-[1.6rem] flex justify-center p-6 sm:p-12 md:w-1/2">
+          <div className="border rounded-l-3xl items-center text-center max-w-4xl flex justify-center p-6 sm:p-12 md:w-1/2">
             <div className="w-full h-[92vh] md:h-96">
-              <div className="flex justify-center items-center">
+              <div className="">
                 <Image
                   src={image2}
                   width="110px"
@@ -99,13 +99,15 @@ const signup = (props) => {
                 </div>
 
                 {/* button */}
-                <button
-                  // onClick={submitFormHandler}
-                  type="submit"
-                  className=" w-[20rem] text-center px-4 py-2 text-sm font-medium text-black transition-colors duration-150 hover:text-[#e6e6e9] border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
-                >
-                  sign-Up
-                </button>
+                <div className="pt-2">
+                  <button
+                    // onClick={submitFormHandler}
+                    type="submit"
+                    className=" w-[20rem] text-center px-4 py-2 text-sm font-medium text-black transition-colors duration-150 hover:text-[#e6e6e9] border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+                  >
+                    sign-Up
+                  </button>
+                </div>
 
                 <p className="text-center text-sm">
                   Already have an account?
