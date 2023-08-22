fcd9e05eaaa1255634ba5d3a8160d401d16efda3
best2025j <audugodtime95@gmai.com>
2023-08-22 17:30:53 +0100

    staffs details added

--- components/UI/Staffs.jsx
+++ components/UI/Staffs.jsx
@@ -12,98 +12,98 @@ const staffData = [
     image: "/mrAyo.jpeg",
     facebook: "https://www.facebook.com/",
     linkedin: "https://www.linkedin.com/",
-    whatsapp: "https://www.whatsapp.com/",
+    whatsapp: "https://wa.me/",
   },
   {
-    name: "Ibrahim",
-    position: "Data-Science Engineer",
+    name: "Ibrahim.o.OYEBARI",
+    position: "Eexcutive director",
     image: "/mrIbrahim.jpeg",
     facebook: "https://www.facebook.com/",
     linkedin: "https://www.linkedin.com/",
-    whatsapp: "https://www.whatsapp.com/",
+    whatsapp: "https://wa.me/",
   },
   {
     name: "Gt",
     position: "Software Engineer",
     image: "/gt1.jpeg",
     facebook: "/",
-    linkedin: "https://www.linkedin.com/",
-    whatsapp: "https://www.whatsapp.com/",
+    linkedin: "https://www.linkedin.com/in/audu-godstime",
+    whatsapp: "https://wa.me/09012902537",
   },
-  // ...
 
-  // {
-  //   name: "John Doe",
-  //   position: "Software Engineer",
-  //   image: "/path/to/image2.jpg",
-  //   facebook: "https://www.facebook.com/",
-  //   linkedin: "https://www.linkedin.com/",
-  //   whatsapp: "https://www.whatsapp.com/",
-  // },
-  // {
-  //   name: "John Doe",
-  //   position: "Software Engineer",
-  //   image: "/path/to/image2.jpg",
-  //   facebook: "https://www.facebook.com/",
-  //   linkedin: "https://www.linkedin.com/",
-  //   whatsapp: "https://www.whatsapp.com/",
-  // },
-  // {
-  //   name: "John Doe",
-  //   position: "Software Engineer",
-  //   image: "/path/to/image2.jpg",
-  //   facebook: "https://www.facebook.com/",
-  //   linkedin: "https://www.linkedin.com/",
-  //   whatsapp: "https://www.whatsapp.com/",
-  // },
-  // {
-  //   name: "John Doe",
-  //   position: "Software Engineer",
-  //   image: "/path/to/image2.jpg",
-  //   facebook: "https://www.facebook.com/",
-  //   linkedin: "https://www.linkedin.com/",
-  //   whatsapp: "https://www.whatsapp.com/",
-  // },
-  // {
-  //   name: "John Doe",
-  //   position: "Software Engineer",
-  //   image: "/path/to/image2.jpg",
-  //   facebook: "https://www.facebook.com/",
-  //   linkedin: "https://www.linkedin.com/",
-  //   whatsapp: "https://www.whatsapp.com/",
-  // },
-  // {
-  //   name: "John Doe",
-  //   position: "Software Engineer",
-  //   image: "/path/to/image2.jpg",
-  //   facebook: "https://www.facebook.com/",
-  //   linkedin: "https://www.linkedin.com/",
-  //   whatsapp: "https://www.whatsapp.com/",
-  // },
-  // {
-  //   name: "John Doe",
-  //   position: "Software Engineer",
-  //   image: "/path/to/image2.jpg",
-  //   facebook: "https://www.facebook.com/",
-  //   linkedin: "https://www.linkedin.com/",
-  //   whatsapp: "https://www.whatsapp.com/",
-  // },
-  // {
-  //   name: "John Doe",
-  //   position: "Software Engineer",
-  //   image: "/path/to/image2.jpg",
-  //   facebook: "https://www.facebook.com/",
-  //   linkedin: "https://www.linkedin.com/",
-  //   whatsapp: "https://www.whatsapp.com/",
-  // },
-  // {
-  //   name: "John Doe",
-  //   position: "Software Engineer",
-  //   image: "/path/to/image2.jpg",
-  //   facebook: "https://www.facebook.com/",
-  //   linkedin: "https://www.linkedin.com/",
-  //   whatsapp: "https://www.whatsapp.com/",
-  // },
+  {
+    name: "Victor Olumati Ajayi",
+    position: " Media Director/ Graphics Designer",
+    image: "/vic.jpeg",
+    facebook: "https://www.facebook.com/",
+    linkedin: "https://www.linkedin.com/",
+    whatsapp: "https://wa.me/09012902537",
+  },
+  {
+    name: "Adeniyi Ogundiran",
+    position: "Business Development Executive / Digital Marketing instructor",
+    image: "/ade1.jpeg",
+    facebook: "https://www.facebook.com/profile.php?id=100078468232214",
+    linkedin: "https://www.linkedin.com/in/ogundiran-adeniyi",
+    whatsapp: " https://wa.me/08145264132",
+  },
+  {
+    name: "Okechukwu Joshua",
+    position:
+      "Business Executive, Project Manager and Assistant Executive to Kingseed NGO.",
+    image: "/igboman.jpeg",
+    facebook: "https://www.facebook.com/",
+    linkedin: "https://www.linkedin.com/in/joshua-okechukwu-0970951b9",
+    whatsapp: "https://wa.me/08131614690",
+  },
+  {
+    name: "Ogundiran Moshood Adeniyi",
+    position: "Busines Development Executive and Digital Marketing Instructor",
+    image: "/vercel.svg",
+    facebook: "https://www.facebook.com/",
+    linkedin: "https://www.linkedin.com/",
+    whatsapp: "https://wa.me/",
+  },
+  {
+    name: "John Doe",
+    position: "Frontend Engineer",
+    image: "/codelord.jpeg",
+    facebook: "http://facebook.com/codelordtx",
+    linkedin: "https://www.linkedin.com/in/codelordx",
+    whatsapp: "https://wa.me/08129740187",
+  },
+  {
+    name: "John Doe",
+    position: "Software Engineer",
+    image: "/path/to/image2.jpg",
+    facebook: "https://www.facebook.com/",
+    linkedin: "https://www.linkedin.com/",
+    whatsapp: "https://wa.me/",
+  },
+  {
+    name: "John Doe",
+    position: "Software Engineer",
+    image: "/path/to/image2.jpg",
+    facebook: "https://www.facebook.com/",
+    linkedin: "https://www.linkedin.com/",
+    whatsapp: "https://wa.me/",
+  },
+  {
+    name: "John Doe",
+    position: "Software Engineer",
+    image: "/path/to/image2.jpg",
+    facebook: "https://www.facebook.com/",
+    linkedin: "https://www.linkedin.com/",
+    whatsapp: "https://wa.me/",
+  },
+  {
+    name: "John Doe",
+    position: "Software Engineer",
+    image: "/path/to/image2.jpg",
+    facebook: "https://www.facebook.com/",
+    linkedin: "https://www.linkedin.com/",
+    whatsapp: "https://wa.me/",
+  },
   // Add more staff data here...
 ];
 
@@ -114,9 +114,9 @@ const Staffs = () => {
     <section>
       <div className="py-40 h-full w-full">
         <h1 className="sm:text-4xl text-2xl font-bold text-center">
-          Meet The Team and Our Professionals
+          Meet the Team and Our Professionals
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 py-6">
           {staffData.map((item, index) => (
             <div
               key={index}
@@ -130,7 +130,7 @@ const Staffs = () => {
                 width={200}
                 height={200}
               />
-              <div className="flex flex-col justify-center items-center p-3">
+              <div className="flex flex-col justify-center items-center py-4">
                 <div className="font-medium text-center">
                   <h1 className="text-sky-500 dark:text-sky-400">
                     {item.name}
